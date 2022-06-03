aec6fd7ecc319d4e0ed161ecc52992a898ee8321
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-06-03 20:39:23 +0530

    Updated Algorithm & Output

--- 18.c
+++ 18.c
@@ -1,5 +1,23 @@
 // Insertion Sort
 
+// Algorithm
+/* 
+1. start
+2. read N as number of items
+3. read N items into the array ARR 
+4. set I = 1
+5. while I < N, repeat steps 5 to 9
+6. set KEY = ARR[I]
+7. set J = I
+8. while J>0 and KEY<ARR[J-1], 
+	8.1. set ARR[J] = ARR[J-1]
+	8.2. decrement J by 1
+9. set ARR[J] = KEY
+10. increment I by 1
+11. print ARR as sorted array
+12. stop
+*/
+
 #include <stdio.h>
 
 void InsertionSort(int a[], int n)
@@ -29,6 +47,13 @@ void main()
 	printf("The sorted elements are : ");
 	for (i = 0; i < n; i++)
 		printf("%d ", a[i]);
-
 	getch();
 }
+
+// Expected output
+/* 
+Enter the number of elements : 6
+Enter the elements :
+23 54 21 13 18 26
+The sorted elements are : 13 18 21 23 26 54
+*/
