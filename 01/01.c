8d5fba242ab31dab69e61d650e73dec7afd5e2ba
~/antony/ashish <72491974+ashishantonytsr@users.noreply.github.com>
2022-12-16 21:38:26 +0530

    Updated algorithm to seperate file

--- 01/01.c
+++ 01/01.c
@@ -1,24 +1,5 @@
 // Reverse of a string using pointer
 
-// Algorithm
-/*
-1. start
-2. declare arrays str, rev & pointers strptr, revptr
-3. assign *strptr = str and *revptr = rev
-4. read the string as str
-5. set i = -1
-6. while *strptr not equal to NULL
-	6.1. increment strptr by  1
-	6.2. increment i by 1
-7. while i>=0
-	7.1. decrement strptr by 1
-	7.2. set *revptr = *strptr
-	7.3. increment revptr by 1
-	7.4. decrement i by 1
-8. print rev as reversed string
-9. stop
-*/
-
 #include <stdio.h>
 void main()
 {
